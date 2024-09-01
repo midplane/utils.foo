f68e87408be3a228d5b668e58c91bf1dc0af9f9a
Nikhil Bafna <nikhil.bafna@phonepe.com>
2024-09-01 22:49:43 +0530

    update style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,19 +7,21 @@ export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <header className="bg-gray-800 text-white p-4">
-        <div className="container mx-auto flex items-center">
-          <Link to="/" className="text-2xl font-bold">utils.foo</Link>
-        </div>
+        <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between lg:px-8">
+          <div className="flex flex-1"></div>
+          <Link to="/" className="text-2xl font-mono">utils.foo</Link>
+          <div className="flex flex-1 justify-end"></div>
+        </nav>
       </header>
       <main className="flex-grow container mx-auto py-8 px-4">
         {children}
       </main>
       <footer className="p-4 mt-8">
         <div className="container mx-auto text-center text-gray-600">
-          <Link to="/" className="ml-4 text-blue-500 hover:text-blue-600">home</Link>
-          <Link to="/faq" className="ml-4 text-blue-500 hover:text-blue-600">faq</Link>
-          <Link to="/privacy" className="ml-4 text-blue-500 hover:text-blue-600">privacy</Link>
-          <Link target='_blank' to="https://github.com/midplane/forum.utils.foo/issues" className="ml-4 text-blue-500 hover:text-blue-600">contact</Link>
+          <Link to="/" className="text-sm leading-6 text-gray-600 hover:text-gray-900">home</Link>
+          <Link to="/faq" className="ml-4 text-sm leading-6 text-gray-600 hover:text-gray-900">faq</Link>
+          <Link to="/privacy" className="ml-4 text-sm leading-6 text-gray-600 hover:text-gray-900">privacy</Link>
+          <Link target='_blank' to="https://github.com/midplane/forum.utils.foo/issues" className="ml-4 text-sm leading-6 text-gray-600 hover:text-gray-900">contact</Link>
         </div>
       </footer>
     </div>
