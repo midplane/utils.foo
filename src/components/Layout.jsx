9641f695d5f3c6feadc5a12c07b781468b308225
Nikhil Bafna <zodvik@gmail.com>
2024-11-18 22:09:42 +0530

    Update forum link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -21,7 +21,7 @@ export default function Layout({ children }) {
           <Link to="/" className="text-sm leading-6 text-gray-600 hover:text-gray-900">home</Link>
           <Link to="/faq" className="ml-4 text-sm leading-6 text-gray-600 hover:text-gray-900">faq</Link>
           <Link to="/privacy" className="ml-4 text-sm leading-6 text-gray-600 hover:text-gray-900">privacy</Link>
-          <Link target='_blank' to="https://github.com/midplane/forum.utils.foo/issues" className="ml-4 text-sm leading-6 text-gray-600 hover:text-gray-900">contact</Link>
+          <Link target='_blank' to="https://github.com/midplane/utils.foo/discussions" className="ml-4 text-sm leading-6 text-gray-600 hover:text-gray-900">contact</Link>
         </div>
       </footer>
     </div>
